4cd10b9518843c63797deb98c214a5e8d27e7411
rightbrainpapi <dahuemaen@gmail.com>
2020-04-25 07:56:24 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,30 @@
+export const config = {
+	// Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+	githubConvertedToken:
+		"Y2Q1MDBlMDE1Zjc0MGVjYzg2YTdmYzg4YzEyNzY0MGI2Y2Y3ZjI4Nw==",
+
+	// Your Github UserName
+	githubUserName: "rightbrainpapi",
+
+	// Your Github Repo Name Where You Have your issues as Blog
+	githubRepo: "cs-hints-blog",
+
+	// Set it to true if you have a Github app to add to this project
+	// and fill the client ID & secret
+	enableOAuth: true,
+	OAuthClientID: "Iv1.700880f732280ec9",
+	OAuthSecret: "0f6ba8af58a9ac85e7c77fa05da345b71a7d46ef",
+
+	// Your Personal Blog Title
+	title: "Darnell Simon",
+
+	// Your Personal Blog Subtitle
+	subtitle: "Software Developer",
+
+	// Header customization
+	header: {
+		backgroundColor: "#f1f6f8", // can be a CSS gradient
+		titleColor: "#ff5252",
+		subtitleColor: "#37474f",
+	},
+};
